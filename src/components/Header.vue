d547a878d5b47847da53aed6a8592885a826258d
TonyBrb <anthonybarbagallo95@gmail.com>
2021-12-01 16:06:07 +0100

    creazione header e main

---
+++ src/components/Header.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <span>BOOLFLIX</span>
+    <div class="search-bar">
+      <input type="text" placeholder="Inserisci il film da cercare">
+      <button class="btn">Cerca</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Header'
+}
+</script>
+
+<style lang="scss" scoped>
+  div{
+    height: 50px;
+    background-color: black;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    span{
+      font-size: 48px;
+      color: red;
+      font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
+    }
+    
+    button{
+      padding: 11px;
+    }
+
+    input{
+      line-height: 35px;
+    }
+  }
+</style>
